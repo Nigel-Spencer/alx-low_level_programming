08fab8552b8977cbfdd20fdfc3afb8b1fcef79eb
Nigel-Spencer <nigelvspencer@gmail.com>
2023-04-29 09:13:46 -0700

    2. File name

---
+++ 0x0D-preprocessor/02-main.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+/**
+ * main - Prints the name of the file
+ *
+ * Return: On success always 0
+ */
+int main(void)
+{
+	printf("%s\n", __FILE__);
+	return (0);
+}
